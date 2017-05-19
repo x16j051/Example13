ffc4916e3e4ceca133e0397cda1d940f262500c6
WIN_NT\x16j051 <x16j051@chiba-fjb.ac.jp>
2017-05-19 12:39:21 +0900

    初回コミット

---
+++ Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Example13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExe = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnExe
+            // 
+            this.btnExe.Location = new System.Drawing.Point(33, 54);
+            this.btnExe.Name = "btnExe";
+            this.btnExe.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btnExe.Size = new System.Drawing.Size(192, 88);
+            this.btnExe.TabIndex = 0;
+            this.btnExe.Text = "繰り返し";
+            this.btnExe.UseVisualStyleBackColor = true;
+            this.btnExe.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnExe);
+            this.Name = "Form1";
+            this.Text = "whileループ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExe;
+    }
+}
+
